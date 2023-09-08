9bfa9f95192dadf4dba6bf1438b9d7d981332f00
Sergio Dobrunoclear <dobryakofff.s@yandex.ruclear>
2023-09-08 21:40:26 +0300

    Задача 52 успешно решена

--- Program.cs
+++ Program.cs
@@ -32,6 +32,73 @@
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+// int SetNumber(string numberName = "")
+// {
+//     Console.Write($"Enter number {numberName}: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     return num;
+// }
+
+// int[,] GetMatrix(int rows, int columns, int minValue, int maxValue)
+//     int[,] matrix = new int[rows, columns];
+
+//     Random random = new Random();
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int l = 0; l < columns; l++)
+//         {
+//             matrix[i, l] = random.Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return matrix;
+
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int l = 0; l < matrix.GetLength(1); l++)
+//         {
+//             System.Console.Write($" {matrix[i, l]} ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// int rows = SetNumber("rows");
+// int columns = SetNumber("columns");
+
+// int min = SetNumber("min value");
+// int max = SetNumber("max value");
+
+// int[,] matrix = GetMatrix(rows, columns, maxValue: max, minValue: min);
+// PrintMatrix(matrix);
+
+// int SetPosition(string position = "") //Запрашиваем индексы числа
+// {
+//     Console.Write($"Enter number {position}: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     return num;
+// }
+
+// int i = SetNumber("i");
+// int l = SetNumber("l");
+
+// if (i <= matrix.GetLength(0) && l <= matrix.GetLength(1))
+// {
+//     System.Console.WriteLine($"число в двумерном массиве = {matrix[i, l]}");
+// }
+// else
+// {
+//     System.Console.WriteLine($"числа с таким индексом в двумерном массиве не существует");
+// }
+
+// System.Console.WriteLine(matrix[i, l]);
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
 int SetNumber(string numberName = "")
 {
     Console.Write($"Enter number {numberName}: ");
@@ -55,6 +122,7 @@ int[,] GetMatrix(int rows, int columns, int minValue, int maxValue) //двуме
 
 }
 
+
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -76,23 +144,14 @@ int max = SetNumber("max value");
 int[,] matrix = GetMatrix(rows, columns, maxValue: max, minValue: min);
 PrintMatrix(matrix);
 
-int SetPosition(string position = "") //Запрашиваем индексы числа
+for (int l = 0; l < matrix.GetLength(1); l++)
 {
-    Console.Write($"Enter number {position}: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    return num;
+    double ArithmeticMean;
+    int SumNumber = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        SumNumber = (SumNumber + matrix[i, l]);
+    }
+    ArithmeticMean = SumNumber / rows;
+    Console.Write(ArithmeticMean + "; ");
 }
-
-int i = SetNumber("i");
-int l = SetNumber("l");
-
-if (i <= matrix.GetLength(0) && l <= matrix.GetLength(1))
-{
-    System.Console.WriteLine($"число в двумерном массиве = {matrix[i, l]}");
-}
-else
-{
-    System.Console.WriteLine($"числа с таким индексом в двумерном массиве не существует");
-}
-
-System.Console.WriteLine(matrix[i, l]);
